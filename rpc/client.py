f9e76f3b8410a0ae8f497f511fd6f3fb333d7639
Arthur Bernardo <arthurbernardo96@hotmail.com>
2019-04-17 19:50:35 -0300

    Inclusão de um pequeno menu

--- rpc/client.py
+++ rpc/client.py
@@ -2,4 +2,16 @@ import rpyc
 
 connection = rpyc.connect('localhost', 18812)
 
-print(connection.root.encrypt('Arthur Bernardo'))
+print("Trabalho de Sistemas Distribuídos – Professora Carla Lara.\n")
+print("\t\t\t MENU")
+print("\t 1 - CRIPTOGRAFAR")
+print("\t 2 - DESCRIPTOGRAFAR\n")
+op = int(input("Escolha a opção desejada:"))
+
+if op == 1:
+    msg = input("Digite a mensagem a ser criptografada:")
+    print(connection.root.encrypt(msg))
+
+if op == 2:
+    msg = input("Digite a mensagem a ser descriptografada:")
+    print(connection.root.encrypt(msg))
